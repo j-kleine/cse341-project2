245c17592cef1737b0036f1ebee3c0fdc7879cbc
j-kleine <joschuadkleine@gmail.com>
2024-07-17 19:01:26 +0200

    attempt to fix missing OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
+const passport = require('passport');
+
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
-router.get('/', (req, res) => {
-    //#swagger.tags=['Hello World']
-    res.send('Hello World');
-});
+// router.get('/', (req, res) => {
+//     //#swagger.tags=['Hello World']
+//     res.send('Hello World');
+// });
 
 router.use('/contacts', require('./contacts'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
